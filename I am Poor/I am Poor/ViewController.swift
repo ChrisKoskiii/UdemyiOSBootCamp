e2837a163eacb5e54ab6f7febbdc9a3b30c36f86
Christopher Koski <82855685+ChrisKoskiii@users.noreply.github.com>
2022-02-18 14:38:01 -0500

    Add files via upload

---
+++ I am Poor/I am Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Poor
+//
+//  Created by Christopher Koski on 2/18/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
