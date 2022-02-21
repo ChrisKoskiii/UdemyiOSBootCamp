6475f208db1234c7c4d58e70d6780692ba78a59f
Christopher Koski <82855685+ChrisKoskiii@users.noreply.github.com>
2022-02-20 21:14:24 -0500

    Add files via upload

---
+++ EggTimer-iOS13/EggTimer/ViewController.swift
@@ -0,0 +1,66 @@
+//
+//  ViewController.swift
+//  EggTimer
+//
+//  Created by Angela Yu on 08/07/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController {
+  @IBOutlet var titleLabel: UILabel!
+  @IBOutlet var progressBar: UIProgressView!
+  
+  var player: AVAudioPlayer?
+  
+  let eggTimes = ["Soft": 3, "Medium": 4, "Hard": 7]
+  
+  var totalTime = 0
+  var secondsPassed = 0
+  
+  var timer = Timer()
+  
+  @IBAction func hardnessSelected(_ sender: UIButton) {
+    timer.invalidate()
+    let hardness = sender.currentTitle!
+    totalTime = eggTimes[hardness]!
+    
+    progressBar.progress = 0.0
+    secondsPassed = 0
+    titleLabel.text = hardness
+    
+    timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+  }
+  
+  @objc func updateTimer() {
+    if secondsPassed < totalTime {
+      secondsPassed += 1
+      progressBar.progress = Float(secondsPassed) / Float(totalTime)
+
+    } else {
+      timer.invalidate()
+      titleLabel.text = "Done!"
+      playSound()
+    }
+  }
+  
+  func playSound() {
+    guard let url = Bundle.main.url(forResource: "alarm_sound", withExtension: "mp3") else { return }
+    
+    do {
+      try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+      try AVAudioSession.sharedInstance().setActive(true)
+      
+      player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
+      
+      guard let player = player else { return }
+      
+      player.play()
+      
+    } catch let error {
+      print(error.localizedDescription)
+    }
+  }
+}
