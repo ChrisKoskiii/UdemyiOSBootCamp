e98dfe2a0b070ebeb9171f8cee10aaef5dcec0f0
Christopher Koski <82855685+ChrisKoskiii@users.noreply.github.com>
2022-02-21 20:29:19 -0500

    Add files via upload

---
+++ ClassesDemo/ClassesDemo/main.swift
@@ -0,0 +1,12 @@
+
+//let skeleton = Enemy()
+//print(skeleton.health)
+//skeleton.move()
+//skeleton.attack()
+
+let dragon = Dragon()
+dragon.wingSpan = 5
+dragon.attackStrength = 15
+dragon.move()
+dragon.attack()
+dragon.talk(speech: "Ahoy there")
